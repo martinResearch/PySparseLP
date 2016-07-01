7bc206428fc882b78f5f7f408a335ae7e2c71459
martin_delagorce <martin_delagorce@localhost>
2016-07-01 08:50:51 +0200

    adding MIT licence on a file

--- create_latex_images.py
+++ create_latex_images.py
@@ -1,3 +1,29 @@
+# -*- coding: utf-8 -*-
+
+# -----------------------------------------------------------------------
+#Copyright © 2016 Martin de la Gorce <martin[dot]delagorce[hat]gmail[dot]com>
+
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT
+# IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+# OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+# ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+# OTHER DEALINGS IN THE SOFTWARE.
+# -----------------------------------------------------------------------
+
+
 # This script will parse you readme.md file and create images for each equation found as 
 # [latex:your_equation](your_image_file)
 #
@@ -6,6 +32,7 @@
 # has the svg text remains unchanged for unchanged euation and thus it avoids pushing again and again the same 
 # images on the server of the aleready compile equations each time a new equation is created 
 # Martin de La Gorce April 2016
+# up to date version of that script can found on https://github.com/martinResearch/markdownLatex
 
 import tempfile
 import shutil
